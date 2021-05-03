2e1893108ee27036493772dc43c7cc9ef668ee0a
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-05-02 23:03:53 -0400

    nav

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export default function Nav(props) {
+  return (
+  <div class="navDiv">
+    <h1> OMDB </h1>;
+    <h1> OMDB </h1>;
+  </div>
+  )
+}
